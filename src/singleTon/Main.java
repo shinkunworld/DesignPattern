f6802d2450d2a30169e46ab8a6dcb028065fa6d9
shinkunworld <shinkunworld@gmail.com>
2019-03-28 21:56:13 +0900

    SingleTon기본포멧

---
+++ src/singleTon/Main.java
@@ -0,0 +1,12 @@
+package singleTon;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FirstClass firstClass = new FirstClass();
+		SecondClass secondClass = new SecondClass();
+
+	}
+
+}
